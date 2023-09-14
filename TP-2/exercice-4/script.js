fe7d17761af84a6ca19874bbbc0d7d20edecad7d
Sandeulo <sandrohen23@gmail.com>
2023-09-14 22:29:52 +0200

    added script

---
+++ TP-2/exercice-4/script.js
@@ -0,0 +1,9 @@
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const color3 = document.querySelector(".color3");
+
+color1.addEventListener("click", function () {});
+
+color2.addEventListener("click", function () {});
+
+color3.addEventListener("click", function () {});
